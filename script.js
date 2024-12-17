5a77fc5f9d8108c73e17f8e2087aa7680cf92e80
f.jonathan9700@gmail.com <106790970+TheUnknown-Programmer@users.noreply.github.com>
2024-12-17 02:58:46 -0500

    buttons show on display

--- script.js
+++ script.js
@@ -1,60 +1,64 @@
-const buttonsArray = [
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "0",
-  ".",
-  "/",
-  "-",
-  "*",
-  "+",
-  "=",
-];
+const numberButtons = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+const operatorButtons = ["/", "-", "*", "+"];
+const specialButtons = ["C", "="];
+const buttonsArray = [...numberButtons, ...operatorButtons, ...specialButtons];
 
 const buttonsContainer = document.getElementById("buttons-container");
+const displayElement = document.getElementById("display");
+displayElement.textContent = "0";
 
-for (let i = 0; i < buttonsArray.length; i++) {
-  const button = document.createElement("button");
-
-  button.textContent = buttonsArray[i];
-
+function addButtonClickListener(button, value) {
   button.addEventListener("click", function () {
-    console.log(`Button ${buttonsArray[i]} clicked!`);
+    displayElement.textContent = value;
+    console.log(value);
   });
-
-  buttonsContainer.appendChild(button);
 }
 
-function add(num1, num2) {
-  let result = 0;
-  result = num1 + num2;
-  return result;
+function buttons() {
+  for (let i = 0; i < buttonsArray.length; i++) {
+    const button = document.createElement("button");
+    button.textContent = buttonsArray[i];
+
+    button.addEventListener("click", function () {
+      if (numberButtons.includes(button.textContent)) {
+        if (displayElement.textContent === "0") {
+          displayElement.textContent = button.textContent;
+        } else {
+          displayElement.textContent += button.textContent;
+        }
+      } else if (operatorButtons.includes(button.textContent)) {
+        displayElement.textContent += ` ${button.textContent} `;
+      } else if (button.textContent === "C") {
+        displayElement.textContent = "0";
+      }
+    });
+
+    buttonsContainer.appendChild(button);
+  }
 }
 
-function subtract(num1, num2) {
-  let result = 0;
-  result = num1 - num2;
-  return result;
-}
+buttons();
 
-function multiply(num1, num2) {
-  let result = 0;
-  result = num1 * num2;
-  return result;
-}
-
-function divide(num1, num2) {
-  let result = 0;
-  result = num1 / num2;
+function operate(firstNumber, operator, secondNumber) {
+  let result;
+  if (operator === "+") {
+    result = firstNumber + secondNumber;
+  } else if (operator === "-") {
+    result = firstNumber - secondNumber;
+  } else if (operator === "*") {
+    result = firstNumber * secondNumber;
+  } else if (operator === "/") {
+    result =
+      secondNumber !== 0
+        ? firstNumber / secondNumber
+        : "Error: Division by zero";
+  } else {
+    result = "Invalid operator";
+  }
   return result;
 }
 
 let firstNumber = 0;
 let operator = "";
 let secondNumber = 0;
+let result = operate(firstNumber, operator, secondNumber);
