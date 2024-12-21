8c546e8df8d9eb8588e3f1e22c635002a4d2e959
f.jonathan9700@gmail.com <106790970+TheUnknown-Programmer@users.noreply.github.com>
2024-12-21 01:47:04 -0500

    Created buttons and added event listners to show in the display

--- script.js
+++ script.js
@@ -7,58 +7,21 @@ const buttonsContainer = document.getElementById("buttons-container");
 const displayElement = document.getElementById("display");
 displayElement.textContent = "0";
 
-function addButtonClickListener(button, value) {
-  button.addEventListener("click", function () {
-    displayElement.textContent = value;
-    console.log(value);
-  });
-}
-
-function buttons() {
+function createButtons() {
   for (let i = 0; i < buttonsArray.length; i++) {
     const button = document.createElement("button");
-    button.textContent = buttonsArray[i];
 
-    button.addEventListener("click", function () {
-      if (numberButtons.includes(button.textContent)) {
-        if (displayElement.textContent === "0") {
-          displayElement.textContent = button.textContent;
-        } else {
-          displayElement.textContent += button.textContent;
-        }
-      } else if (operatorButtons.includes(button.textContent)) {
-        displayElement.textContent += ` ${button.textContent} `;
-      } else if (button.textContent === "C") {
-        displayElement.textContent = "0";
-      }
+    button.textContent = buttonsArray[i];
+    button.addEventListener("click", () => {
+      displayElement.textContent = buttonsArray[i];
     });
 
     buttonsContainer.appendChild(button);
   }
 }
 
-buttons();
-
-function operate(firstNumber, operator, secondNumber) {
-  let result;
-  if (operator === "+") {
-    result = firstNumber + secondNumber;
-  } else if (operator === "-") {
-    result = firstNumber - secondNumber;
-  } else if (operator === "*") {
-    result = firstNumber * secondNumber;
-  } else if (operator === "/") {
-    result =
-      secondNumber !== 0
-        ? firstNumber / secondNumber
-        : "Error: Division by zero";
-  } else {
-    result = "Invalid operator";
-  }
-  return result;
-}
+createButtons();
 
 let firstNumber = 0;
 let operator = "";
 let secondNumber = 0;
-let result = operate(firstNumber, operator, secondNumber);
